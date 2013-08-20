7b087f18663ed8ba2b0cbc7fa6c4f1d19bf1d3ea
neonnds <neonnds@gmail.com>
2013-08-21 05:05:04 +1000

    Shared js to access between components.

---
+++ usvg.js
@@ -0,0 +1,14 @@
+var state;
+var STATE_DISABLED = 0;
+var STATE_ENABLED = 1;
+
+function getState()
+{
+	return state;
+}
+
+function mousedownCallback() {}
+
+function mouseupCallback() {}
+
+function mousemoveCallback() {}
